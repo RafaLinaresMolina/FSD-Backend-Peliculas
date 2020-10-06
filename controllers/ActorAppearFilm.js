b62d8063e4e1872190a5d1dfe81434c86d646765
Rafa Linares <rafa.linares.molina@gmail.com>
2020-10-06 18:36:57 +0200

    fixed many to many relation between Film and Actor

--- controllers/ActorAppearFilm.js
+++ controllers/ActorAppearFilm.js
@@ -1,60 +1,62 @@
 const { Actor, Film, ActorAppearFilm, Sequelize } = require("../models");
 const Op = Sequelize.Op;
 
-ActorAppearFilm.belongsTo(Actor, { foreignKey: "id_actor" });
-ActorAppearFilm.belongsTo(Film, { foreignKey: "id_film" });
+Film.belongsToMany(Actor, {
+  through: { model: ActorAppearFilm },
+  foreignKey: "id_film",
+});
+Actor.belongsToMany(Film, {
+  through: { model: ActorAppearFilm },
+  foreignKey: "id_actor",
+});
 
 const ActorAppearFilmController = {
   async getActorsFromFilmById(req, res) {
     try {
-      const actors = await ActorAppearFilm.findAll({
-        where: {
-          id_film: req.params.id,
-        },
+      const film = await Film.findByPk(req.params.id, {
         include: [
           {
             model: Actor,
             required: true,
+            through: {
+              attributes: [],
+            },
           },
-          {
-            model: Film,
-            required: true,
-          },
-        ]
+        ],
       });
-      console.log(actors);
-      res.send(actors);
+
+      res.send(film);
     } catch (error) {
       console.error(error);
       res
         .status(500)
-        .send({ message: "There was a problem trying to get the Actors from a Film" });
+        .send({
+          message: "There was a problem trying to get the Actors from a Film",
+        });
     }
   },
   async getFilmsByActorId(req, res) {
     try {
-      const actors = await ActorAppearFilm.findAll({
-        where: {
-          id_actor: req.params.id,
-        },
+      const actors = await Actor.findByPk(req.params.id, {
         include: [
           {
             model: Film,
             required: true,
+            through: {
+              attributes: [],
+            },
           },
-          {
-            model: Actor,
-            required: true,
-          },
-        ]
+        ],
       });
-      console.log(actors);
+
       res.send(actors);
     } catch (error) {
       console.error(error);
       res
         .status(500)
-        .send({ message: "There was a problem trying to get the Films from the actor" });
+        .send({
+          message: "There was a problem trying to get the Films from the actor",
+        });
     }
   },
 };
