9c76c12828460ffde022ec33e337702fc7c082be
Rafa Linares <rafa.linares.molina@gmail.com>
2020-10-11 11:51:32 +0200

    Updated actor controller

--- controllers/actor.js
+++ controllers/actor.js
@@ -7,10 +7,11 @@ const ActorController = {
       const actors = await Actor.findAll();
       res.send(actors);
     } catch (error) {
-      console.error(error);
-      res
-        .status(500)
-        .send({ message: "There was a problem trying to get the Films" });
+      procces.log.error(error);
+      res.status(500).send({
+        message: "There was a problem trying to get the Films",
+        trace: err.message,
+      });
     }
   },
   async getById(req, res) {
@@ -18,78 +19,77 @@ const ActorController = {
       const actor = await Actor.findByPk(req.params.id);
       res.send(actor);
     } catch (error) {
-      console.error(error);
-      res
-        .status(500)
-        .send({ message: "There was a problem trying to get the Films" });
+      procces.log.error(error);
+      res.status(500).send({
+        message: "There was a problem trying to get the Films",
+        trace: err.message,
+      });
     }
   },
   async getByName(req, res) {
-    Actor.findAll({
-      where: {
-        name: {
-          [Op.like]: `%${req.params.name}%`,
+    try {
+      const actors = Actor.findAll({
+        where: {
+          name: {
+            [Op.like]: `%${req.params.name}%`,
+          },
         },
-      },
-    })
-      .then((category) => res.send(category))
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send({
-          message: "There was a problem trying to get the Films by name",
-        });
       });
+      res.send(actors);
+    } catch (err) {
+      procces.log.error(err);
+      res.status(500).send({
+        message: "There was a problem trying to get the Films by name",
+        trace: err.message,
+      });
+    }
   },
-  create(req, res) {
-    Actor.create(req.body) //INSERT INTO categories
-      .then((Actor) => res.status(201).send(Actor))
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send({
-          message: "There was a problem trying to create the Actor",
-        });
+  async create(req, res) {
+    try {
+      const actor = await Actor.create(req.body);
+      res.status(201).send(actor); //INSERT INTO categories
+    } catch (err) {
+      procces.log.error(err);
+      res.status(500).send({
+        message: "There was a problem trying to create the Actor",
+        trace: err.message,
       });
+    }
   },
-  update(req, res) {
-    Actor.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    })
-      .then(() =>
-        res.send({
-          message: "Actor successfully updated",
-        })
-      )
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send({
-          message: "There was a problem trying to update the Actor",
-        });
+  async update(req, res) {
+    try {
+      await Actor.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
       });
+      res.send({ message: "Actor successfully updated" });
+    } catch (err) {
+      procces.log.error(err);
+      res.status(500).send({
+        message: "There was a problem trying to update the Actor",
+        trace: err.message,
+      });
+    }
   },
-  delete(req, res) {
-    Actor.destroy({
-      where: {
-        id: req.params.id,
-      },
-    })
-      .then((rowsAffected) => {
-        if (!rowsAffected) {
-          return res.send({
-            message: "Actor not found",
-          });
-        }
-        res.send({
-          message: "Actor successfully removed",
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send({
-          message: "There was a problem trying to remove the Actor",
-        });
+  async delete(req, res) {
+    try {
+      const rowsAffected = await Actor.destroy({
+        where: {
+          id: req.params.id,
+        },
       });
+      if (!rowsAffected) {
+        return res.send({ message: "Actor not found" });
+      }
+      res.send({ message: "Actor successfully removed" });
+    } catch (err) {
+      procces.log.error(err);
+      res.status(500).send({
+        message: "There was a problem trying to remove the Actor",
+        trace: err.message,
+      });
+    }
   },
 };
 
