d1ffb40674a5e8802257201f33225fdba9ec2e7b
Rafa Linares <rafa.linares.molina@gmail.com>
2020-10-11 17:26:31 +0200

    Updated sql scripts with schemas and dummy data

---
+++ sql-scripts/db.sql
@@ -0,0 +1,195 @@
+-- MySQL dump 10.17  Distrib 10.3.22-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: films
+-- ------------------------------------------------------
+-- Server version	10.3.22-MariaDB-1ubuntu1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Actor`
+--
+
+DROP TABLE IF EXISTS `Actor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Actor` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `status` tinyint(1) DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ActorAppearFilm`
+--
+
+DROP TABLE IF EXISTS `ActorAppearFilm`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ActorAppearFilm` (
+  `id_film` int(10) NOT NULL,
+  `id_actor` int(10) NOT NULL,
+  PRIMARY KEY (`id_film`,`id_actor`),
+  KEY `FK_Film_Has_Actor` (`id_actor`),
+  CONSTRAINT `FK_Actor_Appear_Film` FOREIGN KEY (`id_film`) REFERENCES `Film` (`id`),
+  CONSTRAINT `FK_Film_Has_Actor` FOREIGN KEY (`id_actor`) REFERENCES `Actor` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Film`
+--
+
+DROP TABLE IF EXISTS `Film`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Film` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `original_title` varchar(255) NOT NULL,
+  `img_path` varchar(255) NOT NULL,
+  `release_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `synopsis` text NOT NULL,
+  `status` tinyint(1) DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `stock` int(11) DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `FilmIsGenre`
+--
+
+DROP TABLE IF EXISTS `FilmIsGenre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `FilmIsGenre` (
+  `id_film` int(10) NOT NULL,
+  `id_genre` int(10) NOT NULL,
+  PRIMARY KEY (`id_film`,`id_genre`),
+  KEY `FK_Genre_From_Film` (`id_genre`),
+  CONSTRAINT `FK_Film_Is_Genre` FOREIGN KEY (`id_film`) REFERENCES `Film` (`id`),
+  CONSTRAINT `FK_Genre_From_Film` FOREIGN KEY (`id_genre`) REFERENCES `Genre` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Genre`
+--
+
+DROP TABLE IF EXISTS `Genre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Genre` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `status` tinyint(1) DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Order`
+--
+
+DROP TABLE IF EXISTS `Order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Order` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `status` enum('pending','sended','client','returning','stocked') DEFAULT 'pending',
+  `UserId` int(10) DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `arrivedAtClient` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `recomendedReturnDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `realReturnDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `reStoked` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `FK_Order_Has_User` (`UserId`),
+  CONSTRAINT `FK_Order_Has_User` FOREIGN KEY (`UserId`) REFERENCES `User` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `OrderFilm`
+--
+
+DROP TABLE IF EXISTS `OrderFilm`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `OrderFilm` (
+  `FilmId` int(11) NOT NULL,
+  `OrderId` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  PRIMARY KEY (`FilmId`,`OrderId`),
+  KEY `FK_Order_Has_Film` (`OrderId`),
+  CONSTRAINT `FK_Film_Conained_Order` FOREIGN KEY (`FilmId`) REFERENCES `Film` (`id`),
+  CONSTRAINT `FK_Order_Has_Film` FOREIGN KEY (`OrderId`) REFERENCES `Order` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Price`
+--
+
+DROP TABLE IF EXISTS `Price`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Price` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `days` int(2) NOT NULL,
+  `euro_perDay` decimal(3,2) NOT NULL,
+  `status` tinyint(1) DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` tinyint(1) DEFAULT 1,
+  `rol_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_login` timestamp NOT NULL DEFAULT current_timestamp(),
+  `token` text DEFAULT '',
+  `confirmed` tinyint(4) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-11 17:01:08
