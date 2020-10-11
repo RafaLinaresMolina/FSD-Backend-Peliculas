6c23b55f3c57a516f0b7376553cd16c791357df6
Rafa Linares <rafa.linares.molina@gmail.com>
2020-10-11 12:38:54 +0200

    Film controller now use async await

--- controllers/film.js
+++ controllers/film.js
@@ -4,27 +4,29 @@ const Op = Sequelize.Op;
 const FilmController = {
 
   async getFilmByName(req, res) {
-    Film.findAll({
-      where: {
-        [Op.or]: 
-          {
-            original_title: {
-              [Op.like]: `%${req.params.name}%`,
+    try{
+      const films = await Film.findAll({
+        where: {
+          [Op.or]: 
+            {
+              original_title: {
+                [Op.like]: `%${req.params.name}%`,
+              },
+              title: {
+                [Op.like]: `%${req.params.name}%`,
+              },
             },
-            title: {
-              [Op.like]: `%${req.params.name}%`,
-            },
-          },
-        
-      },
-    })
-      .then((category) => res.send(category))
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send({
-          message: "There was a problem trying to get the Films by name",
-        });
+          
+        },
       });
+      res.send(films);
+    }catch(err){
+      process.log.error(err);
+      res.status(500).send({
+        message: "There was a problem trying to get the Films by name",
+        trace: err.message
+      });
+    }
   }
 };
 
